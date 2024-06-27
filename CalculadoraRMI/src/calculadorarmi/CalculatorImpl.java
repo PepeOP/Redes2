781ec6434a87b341b9010571f0e2520e5db45928
PepeOP <150472175+PepeOP@users.noreply.github.com>
2024-06-27 07:32:40 -0600

    Calculadora RMI Version Final

--- CalculadoraRMI/src/calculadorarmi/CalculatorImpl.java
+++ CalculadoraRMI/src/calculadorarmi/CalculatorImpl.java
@@ -11,25 +11,30 @@ public class CalculatorImpl extends UnicastRemoteObject implements Calculator {
 
     @Override
     public double add(double a, double b) throws RemoteException {
+        System.out.println("Servidor: Realizando suma " + a + " + " + b);
         return a + b;
     }
 
     @Override
     public double subtract(double a, double b) throws RemoteException {
+        System.out.println("Servidor: Realizando resta " + a + " - " + b);
         return a - b;
     }
 
     @Override
     public double multiply(double a, double b) throws RemoteException {
+        System.out.println("Servidor: Realizando multiplicación " + a + " * " + b);
         return a * b;
     }
 
     @Override
     public double divide(double a, double b) throws RemoteException {
         if (b == 0) {
-            throw new RemoteException("Division by zero");
+            throw new RemoteException("División por cero");
         }
+        System.out.println("Servidor: Realizando división " + a + " / " + b);
         return a / b;
     }
 }
+
 
